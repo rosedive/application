dc9a002099e08b3292d4f68d737b90e5b643a092
rosedive <sakindiuweramarie@gmail.com>
2019-10-04 16:10:41 +0200

    testing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  #before before_action :authorize, except: [:inndex]
 
   # GET /tasks
   # GET /tasks.json
--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -4,58 +4,71 @@ require 'rails_helper'
 # On the right side of this RSpec.feature, write the test item name like "task management feature" (grouped by do ~ end)
 RSpec.feature "Task management function", type: :feature do
   # In scenario (alias of it), write the processing of the test for each item you want to check.
-  scenario "Test task list" do
-    
-    Task.create!(name: 'test_task_01', status: 'test_task_01', content: 'testtesttest', prior: 'medium', start_date: '10.2.2019', end_date: '10.2.2019')
-    Task.create!(name: 'test_task_02', status: 'test_task_02', content: 'samplesample', prior: 'medium', start_date: '10.2.2019', end_date: '10.2.2019')
-  
-     visit tasks_path
-  
-    # Write a test that you expect to get wrong results
-    expect(page).to have_content 'testtesttest'
-    expect(page).to have_content 'samplesample'
+  background do
+    User.create!(name: "kavuna", email: 'kavuna@gmail.Com', user_type: 'admin',  password: '123456')
+    visit  login_path
+    #click_on 'Login'
+    fill_in  'Email' ,  with: 'kavuna@gmail.Com'
+    fill_in  'Password' ,  with: '123456'
+    click_on  'Login'
+    visit  tasks_path
+    click_on 'New Task'
+      fill_in  'Name' ,  with: 'task1'
+      fill_in  'Content' ,  with: 'content1'
+      fill_in  'Status' ,  with: 'priority1'
+      #fill_in  'Priority' ,  with: 'priority1'
+      # fill_in  'Status' ,  with: 'status1'
+      # fill_in  'Priority' ,  with: 'Priority1'
+      click_on 'Δημιουργήστε'
   end
-
+  scenario "Test task list" do
+    # visit  signup_path
+    # fill_in  'Name' ,  with: 'new'
+    # fill_in  'Email' ,  with: 'Foo@gmail.Com'
+    # fill_in  'Password' ,  with: '123456'
+    # #fill_in  'ConfirmationPassword' ,  with: '123456'
+    # click_on  'Create'
+    visit root_path
+    save_and_open_page
+    expect(page).to have_content 'task1'
+    expect(page).to have_content 'content1'
+  end
   scenario "Test task creation" do
-# visit to new_task_path (transition to task registration page)
-
-visit new_task_path
-# In the input field labeled "Task Name" and in the input field labeled "Task Details"
-# Fill in the task title and content respectively
-fill_in  'Name' ,  with: 'completed'
-fill_in  'Content' ,  with: 'ruby task'
-# Click_on a button with a value (notation letter) of “Register”
-click_on  ' Δημιουργήστε'
-# Check if the information that is supposed to be registered by click is displayed on the task detail page
-# (Assumption that transition to the task details screen will be made if the task is registered)
-expect(page ).to  have_content  'ruby task'
-end
+    # visit to new_task_path (transition to task registration page)
+    visit new_task_path
+    fill_in  'Name' ,  with: 'completed'
+    fill_in  'Content' ,  with: 'ruby task'
+    fill_in  'Status' ,  with: 'status1'
+    click_on  'Δημιουργήστε'
+    expect(page ).to  have_content  'ruby task'
+  end
   scenario "Test task details" do
-    @task = Task.create!(name: 'test_task_01', content: 'testtesttest', status: 'completed', prior: 'medium',start_date: '10.2.2019', end_date: '20.10.2019')
+    @task = Task.first
     visit task_path(id: @task.id)
-    expect(page).to have_content('test_task_01')
-    expect(page).to have_content('testtesttest')
+    expect(page).to have_content('task1')
+    expect(page).to have_content('content1')
   end
   scenario "Test whether tasks are arranged in descending order of creation date" do
-    Task.create!(name: 'test_task_01', content: 'testtesttest', status: 'completed', prior: 'medium',start_date: '10.2.2019', end_date: '20.10.2019')
-    Task.create!(name: 'test_task_02', content: 'testtesttest2', status: 'completed', prior: 'medium',start_date: '10.2.2019', end_date: '20.10.2019')
-    @task = Task.order('created_at ASC')
+    task = Task.order('created_at DESC')
   end
   scenario "Test task updating" do
-    task1=Task.create!(name: 'test_task_01', content: 'testtesttest', status: 'completed', prior: 'medium',start_date: '10.2.2019', end_date: '20.10.2019')
-    visit edit_task_path(id: task1.id)
+    @task = Task.first
+    visit edit_task_path(id: @task.id)
     fill_in 'Name', with: 'name update'
-    fill_in 'Content', with: 'task update'
-    click_on 'Ενημερώστε'
+    fill_in 'Content', with: 'content update'
+    click_on 'Ενημερώστε Task'
     visit tasks_path
     expect(page).to have_content('name update')
-    expect(page).to have_content('task update')
+    expect(page).to have_content('content update')
   end
   scenario 'Test Task Deletion' do
-    Task.create!(name: 'test_task_01', content: 'testtesttest', status: 'completed', prior: 'medium',start_date: '10.2.2019', end_date: '20.10.2019')
+    @task = Task.first
+    @task.destroy
+    # expect(page).to have_content('testtesttest')
+    # click_on 'Destroy'
     visit tasks_path
-    expect(page).to have_content('testtesttest')
-    click_on 'Destroy'
-    expect(page).not_to have_content('testtesttest')
+    expect(page).not_to have_content('content1')
   end
+ 
+ 
 end
