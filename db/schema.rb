bb3851fe156a86a8be75f2c3cba8cbc510053445
rosedive <sakindiuweramarie@gmail.com>
2019-10-18 16:00:25 +0200

    generate migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_124855) do
+ActiveRecord::Schema.define(version: 2019_10_18_135118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(version: 2019_10_03_124855) do
     t.string "name"
     t.string "email"
     t.string "user_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
   end
 
